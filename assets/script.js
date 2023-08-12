07c034c872c6862ed33c343b637b80d3d1d45769
Hasibul <skhasibulhossain16@gmail.com>
2023-08-12 15:25:16 +0530

    add api and add functionalities sort by rating fixed

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,66 @@
-const movieListSection = document.querySelector("#movie-list");
+let pageNumber;
+let totalPages = 0;
+let currentState = "desc";
 
-async function showMovies(pageNumber = 1) {
-  const response = await fetch();
-  // `https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${pageNumber}`
+const movieListSection = document.querySelector("#movie-list");
+const nextBtn = document.querySelector("#next");
+const backBtn = document.querySelector("#prev");
+const pageNumberContainer = document.querySelector("#page-no");
+const ratingToggle = document.querySelector("#rating-toggle");
+
+const SORT_ASC_TEXT = "Sort by rating descending";
+const SORT_DESC_TEXT = "Sort by rating ascending";
+
+const SORT_ASC = "popularity.asc";
+const SORT_DESC = "popularity.desc";
+
+function addNavigationButtons() {
+  nextBtn.addEventListener("click", () => {
+    if (pageNumber < totalPages) {
+      pageNumber++;
+      showMovies(pageNumber, currentState);
+    }
+  });
+
+  backBtn.addEventListener("click", () => {
+    if (pageNumber > 1) {
+      pageNumber--;
+      showMovies(pageNumber, currentState);
+    }
+  });
+}
+
+function addPopularityButton() {
+  ratingToggle.addEventListener("click", (e) => {
+    currentState = currentState === "desc" ? "asc" : "desc";
+    pageNumber = 1;
+    showMovies(pageNumber, currentState);
+    e.target.innerText =
+      currentState === "desc" ? SORT_DESC_TEXT : SORT_ASC_TEXT;
+  });
+}
+
+async function showMovies(pageNumber = 1, sort_by = "desc") {
+  movieListSection.innerText = "";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjE1ZDM3YTczYWQ5MzVlMmExZjg4N2ZhNTllMmY1ZiIsInN1YiI6IjY0ZDRiYjZmZGI0ZWQ2MDBlMmI1ODQ5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qTL5AjBr_twKgRjFibgHEIv1OLlrigLLkyTkTzlV2OI",
+    },
+  };
+  let response;
+
+  response = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en-US&page=${pageNumber}&sort_by=${
+      sort_by === "asc" ? SORT_ASC : SORT_DESC
+    }`,
+    options
+  );
+
   const json = await response.json();
+  totalPages = json.total_pages;
   const movieList = json.results;
   for (let movie of movieList) {
     // movie section
@@ -17,7 +74,7 @@ async function showMovies(pageNumber = 1) {
     movieDetails.classList.add("movie-details");
     // image/ banner
     let banner = document.createElement("img");
-    banner.src = `https://image.tmdb.org/t/p/w200/${movie.backdrop_path}`;
+    banner.src = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
     banner.classList.add("movie-poster");
     // footer
     const footer = document.createElement("footer");
@@ -34,9 +91,18 @@ async function showMovies(pageNumber = 1) {
     movieElement.appendChild(movieTitle);
     movieElement.appendChild(footer);
     movieListSection.appendChild(movieElement);
+    pageNumberContainer.innerText = pageNumber;
   }
 
   //   movieList
 }
 
-showMovies();
+async function init() {
+  pageNumber = 1;
+  await showMovies(1);
+  addNavigationButtons();
+  addPopularityButton();
+}
+
+// application code
+init();
